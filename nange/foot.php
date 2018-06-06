e50688476e04506ac0512423a1021d6b11d1c588
徐佩楠 <i@nais.me>
2018-06-07 01:52:24 +0800

    Add files via upload

---
+++ nange/foot.php
@@ -0,0 +1,3 @@
+<div id="footer">Copyright &copy; <span style="color:#999999">唯美女生</span>, Powered by
+			<br/><a href="http://www.vmgirls.com" target="_blank" style="color:#999999">唯美女生版权所有</a>
+</div> 
---
+++ nange/inc.php
@@ -0,0 +1,5 @@
+<title>唯美女生解析管理后台</title>
+<meta name="keywords" content="唯美女生" />
+<meta name="description" content="唯美女生" />
+<link href="./images/admin.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="./../favicon.ico"><?php 
